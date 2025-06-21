0a7266f84ff0e186d9971ab8452f10b0d512a85f
Jean Lima <JeanCarlosOliveira58@gmail.com>
2025-06-21 15:40:46 +0000

    Error: Hydration failed because the server rendered HTML didn't match th

--- src/components/page-footer.tsx
+++ src/components/page-footer.tsx
@@ -5,7 +5,7 @@ export default function PageFooter() {
         <h2 className="font-display text-3xl md:text-4xl lg:text-5xl font-light italic">Com amor, Eloisa & Jean</h2>
         <div className="w-full pt-8 border-t border-background/20">
             <p className="text-sm opacity-60 font-light">
-                Criado e desenvolvido por Jean Lima, todos os direitos reservados. © {new Date().getFullYear()}
+                Criado e desenvolvido por Jean Lima, todos os direitos reservados. © 2026
             </p>
         </div>
       </div>
