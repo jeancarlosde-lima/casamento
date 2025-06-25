724e47316fee7eac9c33055456134d3dd324e139
Jean Lima <JeanCarlosOliveira58@gmail.com>
2025-06-25 01:24:16 +0000

    perfeito, troque a imagem por esse link agora, por favor:
    https://imgur

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,7 @@ export default {
     extend: {
       backgroundImage: {
         'hero-mobile': "url('https://i.imgur.com/BUZYlpb.jpg')",
-        'hero-desktop': "url('https://i.imgur.com/TitKk70.jpg')",
+        'hero-desktop': "url('https://i.imgur.com/jhjjqj7.jpg')",
       },
       fontFamily: {
         sans: ['var(--font-sans)', 'serif'],
