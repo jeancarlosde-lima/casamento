3ee9829e92403dea1cd660c74a59adb485214325
Jean Lima <JeanCarlosOliveira58@gmail.com>
2025-11-20 20:01:44 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "projectId": "studio-4205786983-1fb47",
+  "appId": "1:202079827350:web:cc56552eb0628332f399b7",
+  "apiKey": "AIzaSyD_mr1fqzqZarNyuaHZrdSRPJVdMYt5KCo",
+  "authDomain": "studio-4205786983-1fb47.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "202079827350"
 };
 
 // Initialize Firebase
